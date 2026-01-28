95f366bcff2fda3c7c6393eef294d5b91e75c51f
Imaanparvez <134902899+Imaanparvez@users.noreply.github.com>
2026-01-28 22:07:22 +0530

    Add user registration and login functionality
    Implement user registration and login functions with password hashing.

---
+++ backend/auth.py
@@ -0,0 +1,28 @@
+from db import users_col
+import bcrypt
+
+def register_user(name, email, phone, password):
+    if users_col.find_one({"email": email}):
+        return False
+
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+    users_col.insert_one({
+        "name": name,
+        "email": email,
+        "phone": phone,
+        "password": hashed
+    })
+
+    return True
+
+
+def login_user(email, password):
+    user = users_col.find_one({"email": email})
+    if not user:
+        return None
+
+    if bcrypt.checkpw(password.encode(), user["password"]):
+        return user
+
+    return None
