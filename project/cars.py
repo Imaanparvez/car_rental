b892cfa4c47843350044a9832cfb6dc2a682b88a
Imaanparvez <134902899+Imaanparvez@users.noreply.github.com>
2026-01-28 22:09:37 +0530

    Delete project directory

--- project/cars.py
+++
@@ -1,8 +0,0 @@
-from db import cars_col
-
-def get_all_cars():
-    cars = []
-    for c in cars_col.find():
-        c["_id"] = str(c["_id"])  # Convert ObjectId to string
-        cars.append(c)
-    return cars
