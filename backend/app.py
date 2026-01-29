ad5be6806837c906bc74836eadabab52fa6c44ea
unknown <v1hnuk17@gmail.com>
2026-01-29 18:12:13 +0530

    updated

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from auth import login_user, register_user
@@ -119,10 +120,9 @@ def home():
 # -----------------------------------------------------
 # RUN
 # -----------------------------------------------------
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
 
-# RUN
-# -----------------------------------------------------
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
