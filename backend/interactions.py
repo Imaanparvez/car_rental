baf1ff72aefcec77ecf799d18d6531f572ec4ff8
unknown <v1hnuk17@gmail.com>
2026-03-07 21:07:38 +0530

    updated

--- backend/interactions.py
+++ backend/interactions.py
@@ -1,18 +1,10 @@
 from db import interactions_col
 from datetime import datetime
 
-def log_interaction(user_id, car_id, action):
-    """
-    Log user interaction with a car.
-    Allowed actions: view, click, booked
-    """
-
-    if action not in ["view", "click", "booked"]:
-        raise ValueError("Invalid action type")
-
+def log_booking_interaction(user_id, car_id):
     interactions_col.insert_one({
         "user_id": user_id,
         "car_id": car_id,
-        "action": action,
+        "action": "booked",
         "timestamp": datetime.utcnow()
     })
